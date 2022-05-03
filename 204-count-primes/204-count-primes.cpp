791eca15c5b96c87957343a25280a6a45cd7b49d
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-05-03 17:54:32 +0530

    Time: 193 ms (82.96%), Space: 10.3 MB (72.22%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<bool> isPrime(n+1 , true) ;
-        
+        vector<bool> isPrime(n, true) ;  //we have taken n because we want prime numbers that are                                                //                     strictly less than n.
         int count = 0 ;
         for(int i=2 ; i*i<n ; i++){
             if(isPrime[i]){
@@ -15,7 +14,6 @@ public:
                 count++ ;
         }
         return count ;
-
     }
 };
 

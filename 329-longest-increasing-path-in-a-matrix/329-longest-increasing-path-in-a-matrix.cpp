0524437e86ad92378d81516df03d37f8dd022fc5
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-05-19 23:46:38 +0530

    Time: 62 ms (55.34%), Space: 15.8 MB (82.99%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
         bool checkLimits(int i, int j, int n, int m){
         
-        return (i>=0 and i<n and j>=0 and j<m);
-        
-        
+            return (i>=0 and i<n and j>=0 and j<m);  
     }
     
     int func(vector<vector<int>> &matrix, vector<vector<int>> &dp, int i, int j, int n, int m){

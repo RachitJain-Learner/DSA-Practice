e92db4c8ccf76c5372fe0aee04c3c826a3f0ef79
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-06-01 15:14:46 +0530

    Time: 3 ms (59.12%), Space: 8.2 MB (90.94%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        int n = nums.size() ;
+        
+        //BUBBLE SORT
+        bool flag = true ;
+        
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<n-i-1 ;j++){
+                if(nums[j] > nums[j+1]){
+                    swap(nums[j], nums[j+1]) ;
+                    flag = false ;
+                }
+            }
+            if(flag)
+                    break ;
+        }
+    }
+};

831f853ee578cb825891a4b1d94d89430475f7c1
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-06-04 16:16:46 +0530

    Time: 78 ms (27.34%), Space: 51.1 MB (24.61%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,28 @@
+#define pb push_back
+
+class Solution {
+    void helper(int n, int k, int i , vector<int>subSet, vector<vector<int>>& ans){
+        int l = subSet.size() ; 
+        if(subSet.size() == k){            //base case 1
+            ans.pb(subSet) ;
+            return ;
+        }
+        if(k-l > n-i+1) return ;
+        if(i>n) return ;
+        
+        helper(n , k , i+1 , subSet , ans) ;   //exclude 
+        
+        subSet.push_back(i) ;                  //include
+        helper(n , k , i+1 , subSet , ans) ;
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        
+        vector<int>subSet ; 
+        vector<vector<int>> ans ;
+        int i = 1 ;
+        helper(n , k , i , subSet , ans) ;
+        
+        return ans ;
+    }
+};

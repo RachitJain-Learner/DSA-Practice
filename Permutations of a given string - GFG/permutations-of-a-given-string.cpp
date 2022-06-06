737612cacf6f725cb9a2722690b8719147aa553a
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-06-07 00:19:03 +0530

    Added solution - LeetHub

---
+++ Permutations of a given string - GFG/permutations-of-a-given-string.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{   
+    void solve(string S , int pos , vector<string>& ans){
+        
+        unordered_set<char> s;
+        
+        if(pos == S.length()){
+            ans.push_back(S) ;
+            return ;
+        }
+        for(int i = pos; i<S.length() ; i++){
+            if(s.count(S[i]))
+                break ;
+            
+            s.insert(S[i]) ;
+            swap(S[pos] , S[i]) ;
+            solve(S, pos+1, ans) ;
+        }
+    }
+	public:
+		vector<string>find_permutation(string S)
+		{
+		    vector<string>ans ;
+		    int i = 0 ;
+		    solve(S , i , ans);
+		    sort(ans.begin() , ans.end()) ;
+		    return ans ;
+		}
+};
+
+
+
+// { Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--)
+    {
+	    string S;
+	    cin >> S;
+	    Solution ob;
+	    vector<string> ans = ob.find_permutation(S);
+	    for(auto i: ans)
+	    {
+	    	cout<<i<<" ";
+	    }
+	    cout<<"\n";
+    }
+	return 0;
+}
+  // } Driver Code Ends

e92d91ed29869a23c8a61a4a5dce0e3e6b65879f
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-06-07 08:38:38 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (46.32%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -3,18 +3,12 @@ class Solution {
     vector<vector<int>> ansArr;
     
 public:
-    int sum(vector<int>& combination){
-        int ans = 0;
-        for(auto x: combination){
-            ans += x;
-        }
-        return ans;
-    }
     
-    void combinationSumHelper(int k, int n, vector<int>& combination, int num){
+    void combinationSumHelper(int k, int n, int sum, vector<int>& combination, int num){
         
+        if(sum > n) return ;
         if(k == 0){
-            if(sum(combination) == n ){
+            if(sum == n){
                 ansArr.push_back(combination);
             }
             return;
@@ -22,17 +16,20 @@ public:
         
         if(num > 9) return;
         
+        sum += num ; 
         combination.push_back(num);  // Including num in combination
-        combinationSumHelper(k - 1, n, combination, num + 1);
+        combinationSumHelper(k - 1, n, sum, combination, num + 1);
         combination.pop_back(); // Not including num in combination
-        combinationSumHelper(k, n, combination, num + 1);    
+        sum -= num ;
+        
+        combinationSumHelper(k, n, sum, combination, num + 1);    
         
     }
     
     vector<vector<int>> combinationSum3(int k, int n) {
         
         vector<int> combination;
-        combinationSumHelper(k, n, combination, 1);
+        combinationSumHelper(k, n, 0, combination, 1);
         return ansArr;
          
     }

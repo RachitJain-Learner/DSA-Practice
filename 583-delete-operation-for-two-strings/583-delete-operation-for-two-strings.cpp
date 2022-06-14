772b7c8cc5d606a42a03dd068430a701d51ee3a5
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-06-14 23:40:51 +0530

    Time: 14 ms (84.46%), Space: 7.4 MB (91.15%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-        
-        int m=word1.length(), n=word2.length(); 
-        
-        vector<vector<int>> dp(m+1, vector<int> (n+1, 0)); 
-        
-        for(int i=0; i<=m; i++)
-        {
-            for(int j=0; j<=n; j++)
-            {
-                if(i==0 || j==0)
-                    dp[i][j] = i+j;
-                
-                else if(word1[i-1]==word2[j-1])
-                    dp[i][j] = dp[i-1][j-1];
-                
-                else
-                    dp[i][j] = 1+min(dp[i-1][j], dp[i][j-1]);
+    int lcs(string X, string Y,int m,int n){
+        int dp[m+1][n+1];
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0 || j==0) dp[i][j]=0;
             }
         }
-        return dp[m][n];  
+        
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(X[i-1]==Y[j-1]) dp[i][j]= 1+dp[i-1][j-1];
+                else dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+        return dp[m][n];
+    }
+    
+    int minDistance(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+        return m+n-2*lcs(s1,s2,m,n) ;
     }
 };

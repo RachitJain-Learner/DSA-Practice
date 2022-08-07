23836a534e36babfbf5c4e9c3d8f7a3e69b4d082
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-08-07 23:24:46 +0530

    Time: 10 ms (75.42%), Space: 7.6 MB (36.27%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -18,17 +18,16 @@ public:
         while(fast && fast->next){
             slow = slow->next ;
             fast = fast->next->next ;
-            if(fast == slow)
-                break ;
-        }
-        if(fast == slow){
-            slow = head ;
             
-            while(slow != fast){
-                slow = slow->next ;
-                fast = fast->next ;
+            if(fast == slow){
+                slow = head ;
+
+                while(slow != fast){
+                    slow = slow->next ;
+                    fast = fast->next ;
+                }
+                return slow ;
             }
-            return slow ;
         }
         return NULL ;
     }

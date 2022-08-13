e8d72649cab9890b6fde6185490dd41d4615b5d4
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-08-13 11:59:30 +0530

    Time: 15 ms (53.68%), Space: 17.3 MB (52.35%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -14,23 +14,27 @@ public:
     int widthOfBinaryTree(TreeNode* root) {
         if(!root)
             return 0 ;
+        
         int ans = 0 ;
+        
         queue<pair<TreeNode*, int>> nodes ;
-        nodes.push({root, 0}) ;
+        nodes.push({root, 0}) ;            // 0 based indexing
         
         while(!nodes.empty()){
             int size = nodes.size() ;
-            int id = nodes.front().second ; 
+           // int cur_id = nodes.front().second ; 
             int first, last ;
             
             for(int i=0 ; i<size ; ++i){
-                int cur_id = nodes.front().second - id ;
+                int cur_id = nodes.front().second ; //- id ;
+                
                 TreeNode *tmp = nodes.front().first ;
                 nodes.pop() ;
+                
                 if(i == 0) first = cur_id ; 
                 if(i == size-1) last = cur_id ;
                 
-                if(tmp->left) nodes.push({tmp->left, 2LL*cur_id+1}) ;
+                if(tmp->left) nodes.push({tmp->left, 2LL*cur_id+1}) ; //LL-->working like long long int
                 if(tmp->right) nodes.push({tmp->right, 2LL*cur_id+2}) ;
             }
             ans = max(ans, last-first+1) ;

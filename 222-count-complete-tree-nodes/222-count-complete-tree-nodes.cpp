ae7f6a856f52775854c5cb76055d51a326be60b2
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-08-16 00:27:18 +0530

    Time: 73 ms (12.00%), Space: 31 MB (20.99%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -10,15 +10,29 @@
  * };
  */
 class Solution {
- 
+    int heightLeft(TreeNode* root){
+        if(!root)
+            return 0 ;
+        
+        return 1 + heightLeft(root->left) ; ;
+    }
+    int heightRight(TreeNode* root){
+        if(!root)
+            return 0 ;
+        
+        return 1 + heightRight(root->right) ;
+    }
 public:
     int countNodes(TreeNode* root) {
         if(root == NULL)
-            return 0;
+            return 0 ;
         
-        int leftH = countNodes(root->left) ; 
-        int rightH = countNodes(root->right) ;
-
-        return 1 + leftH + rightH ;
+        int leftH = heightLeft(root->left) ; 
+        int rightH = heightRight(root->right) ;
+        
+        if(leftH == rightH)
+            return pow(2, leftH+1) - 1 ;
+        
+        return 1 + countNodes(root->left) + countNodes(root->right) ;
     }
 };

1eee47fa4aead0b17ceb9e1d9e7ce270e71b6eb2
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-08-23 00:50:32 +0530

    Time: 68 ms (6.56%), Space: 23.2 MB (65.03%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,19 +11,14 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-                                            // no need to write case of root == NULL
-        while(true){                        //since given that p and q exist in BST
-            int curr = root->val ;
+        
+        int curr = root->val ;
             
-            if(curr > p->val && curr > q->val){
-                root = root->left ;              // if(root->left) the traverse cond is not 
-            }                           //required since given that both p and q exist in BST 
-            
-            else if(curr < p->val && curr < q->val){
-                root = root->right ;                   // if(root->right) same reason
-            }
-            else
-                break ;
+        if(curr > p->val && curr > q->val){
+            return lowestCommonAncestor(root->left, p, q) ;
+        }
+        else if(curr < p->val && curr < q->val){
+            return lowestCommonAncestor(root->right, p, q) ;
         }
         return root ;
     }

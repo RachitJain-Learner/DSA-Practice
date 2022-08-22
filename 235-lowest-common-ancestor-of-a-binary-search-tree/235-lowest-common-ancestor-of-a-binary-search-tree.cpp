760bad1deb11849b45395c72246e33bb2eba49a2
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-08-23 00:50:52 +0530

    Time: 68 ms (6.56%), Space: 23.3 MB (13.83%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -17,7 +17,7 @@ public:
         if(curr > p->val && curr > q->val){
             return lowestCommonAncestor(root->left, p, q) ;
         }
-        else if(curr < p->val && curr < q->val){
+        if(curr < p->val && curr < q->val){
             return lowestCommonAncestor(root->right, p, q) ;
         }
         return root ;

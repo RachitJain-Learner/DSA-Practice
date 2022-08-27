9a591efa6134d070ba5e3b995bf956873f216023
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-08-27 23:45:17 +0530

    Time: 7 ms (75.00%), Space: 11.3 MB (8.33%) - LeetHub

--- 2383-minimum-hours-of-training-to-win-a-competition/2383-minimum-hours-of-training-to-win-a-competition.cpp
+++ 2383-minimum-hours-of-training-to-win-a-competition/2383-minimum-hours-of-training-to-win-a-competition.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     int minNumberOfHours(int initialEnergy, int initialExperience, vector<int>& energy, vector<int>& experience) {
-        int ans1 = 0 , ans2 = 0 ;
+        int ans = 0 , ans2 = 0 ;
         
         int sum = accumulate(energy.begin(), energy.end(), 0);
         
-        ans1 = (sum >= initialEnergy)? sum - initialEnergy + 1 : 0 ;
+        if(sum >= initialEnergy) 
+            ans = sum - initialEnergy + 1 ;
         
         for(int i=0 ; i < experience.size() ; i++){
-            if(initialExperience > experience[i]){
+            
+            if(initialExperience > experience[i])
                 initialExperience += experience[i] ;
-            }
+            
             else{
                 ans2 = experience[i] - initialExperience + 1 ;
                 initialExperience += experience[i] + ans2 ;
-                ans1 += ans2 ;
+                ans += ans2 ;
             }
         }
-        return ans1 ; 
+        return ans ; 
     }
 };

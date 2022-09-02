f16c39d362e6a45284c6b0c97e41cc8ab9643dd0
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-09-02 09:35:03 +0530

    Time: 24 ms (39.69%), Space: 11 MB (27.78%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,25 +1,24 @@
 class Solution {
-    int n ;
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        n = nums.size();
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> ans; 
-        ans.push_back(nums);
-        while(next_permu(nums))
-            ans.push_back(nums);
-        return ans;
-    }
-    
-    bool next_permu(vector<int> &nums){
-        int i ;
-        for(i = n - 2; i >= 0 and nums[i] >= nums[i+1]; i--);
-        if(i < 0) return false;
-        int j;
-        for(j = n - 1; j >= 0 and nums[j] <= nums[i]; j--);
+    void solve(int pos, vector<int>& nums, vector<vector<int>>& ans){
+        if(pos >= nums.size())
+            ans.push_back(nums) ;
         
-        swap(nums[j],nums[i]);        
-        reverse(nums.begin() + i + 1, nums.end());
-        return true;
+        unordered_set<int> s;
+        
+        for(int i=pos ; i<nums.size() ; ++i){
+            if(s.find(nums[i]) == s.end()){ //if(s.find(nums[i]) != s.end())continue 
+                s.insert(nums[i]) ;
+                
+                swap(nums[pos], nums[i]) ;
+                solve(pos+1, nums, ans) ;
+                swap(nums[pos], nums[i]) ;
+            }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans; 
+        solve(0, nums, ans) ;
+        return ans;
     }
 };

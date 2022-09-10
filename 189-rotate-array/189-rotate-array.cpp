de983be7263a2c46e796bd1ab134e85a9e9a9252
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-09-10 15:37:07 +0530

    Time: 75 ms (7.24%), Space: 25.5 MB (37.89%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,10 +3,13 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size() ;
         vector<int> temp(n) ;
-        
-        for(int i=0 ; i<n ; ++i){
-            temp[(i+k)%n] = nums[i] ; 
+        k = k % n ;
+        int i ;
+        for(i=0 ; i<k ; ++i){
+            temp[i] = nums[n-k+i] ; 
         }
+        for(int j=0 ; j<n-k ; j++)
+            temp[i+j] = nums[j] ; 
         nums = temp ;
     }
 };

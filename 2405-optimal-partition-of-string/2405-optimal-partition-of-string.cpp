4d04dd3453319fb7d7a76d9d07d8a2a10171762e
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-09-11 11:27:35 +0530

    Time: 230 ms (20.00%), Space: 44.6 MB (20.00%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int partitionString(string s) {
-        unordered_set<char> t ;
+        unordered_set<char> set ;
         int ans = 1 ;
-        
         for(int i=0 ; i<s.length() ; ++i){
-            if(t.find(s[i]) != t.end()) {
-                ans++ ;
-                t.clear();
+            if(set.find(s[i]) != set.end()) {      
+                ans++ ;                // if duplicate char is found then increment the ans & clear the set
+                set.clear();    
             }
-            t.insert(s[i]) ;
+            set.insert(s[i]) ;         // insert the char in the set
         }
         return ans ;
     }
 };
-
-
-

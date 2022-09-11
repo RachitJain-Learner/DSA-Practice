6511ab78f26ffd29946b0d99d2e3bf92cbbd3f39
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-09-11 10:28:15 +0530

    Time: 508 ms (20.00%), Space: 44.6 MB (20.00%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
 
     int partitionString(string s) {
-        set<char> t ;
+        unordered_set<char> t ;
         int ct = 1 ;
         for(int i=0 ; i<s.length() ; ++i){
             
-            if(t.find(s[i]) == t.end()){
-                
-               t.insert(s[i]);
-            }
+            if(t.find(s[i]) == t.end())
+                t.insert(s[i]);
+            
             else{
+                ct++ ;
                 t.clear();
                 t.insert(s[i]) ;
-                ct++ ;
-                
             }
         }
-        
         return ct ;
     }
 };

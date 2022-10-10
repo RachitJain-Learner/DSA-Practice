0493bd14089e8d3d31f3e25984176ea29984498c
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-10-11 00:27:07 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (78.51%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string breakPalindrome(string p) {
+         int sz = p.size();
+         if (sz <= 1) return "";
+        
+        for (size_t i=0; i < sz/2; i++) {
+            if (p[i] > 'a')  {
+                p[i] = 'a';
+                return p;
+            }
+        }
+        
+        p[sz-1] = 'b';
+        return p;
+    }
+};

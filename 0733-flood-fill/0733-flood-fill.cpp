b93865d2a31cce5f47ccf21832e7b6f38aa069be
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-10-21 19:57:25 +0530

    Time: 22 ms (30.47%), Space: 14.1 MB (47.85%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,24 +1,25 @@
 class Solution {
 private:
-    void dfs(int sr, int sc, vector<vector<int>>& image, vector<vector<int>>& copy, int start, int color, int drow[], int dcol[]){
-        copy[sr][sc] = color ;
+    void dfs(int r, int c, vector<vector<int>>& image, vector<vector<int>>& ans, int startColor, int newColor, int drow[], int dcol[]){
+        ans[r][c] = newColor ;
 
         for(int i= 0 ; i < 4 ; ++i){
-            int nrow = sr + drow[i] ;
-            int ncol = sc + dcol[i] ;
-            if(nrow >= 0 && nrow < image.size()  && ncol >= 0 && ncol < image[0].size() && image[nrow][ncol] == start && 
-               copy[nrow][ncol] != color){
-                dfs(nrow, ncol, image, copy, start, color, drow, dcol);
+            int nrow = r + drow[i] ;
+            int ncol = c + dcol[i] ;
+            if(nrow >= 0 && nrow < image.size()  && ncol >= 0 && ncol < image[0].size() && 
+               image[nrow][ncol] == startColor && ans[nrow][ncol] != newColor){  
+                                                                    // Another solution in notes --->
+                dfs(nrow, ncol, image, ans, startColor, newColor, drow, dcol);
             }
         }
-    }
+    }                                                          
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int start = image[sr][sc] ;
-        vector<vector<int>>copy = image ;
+        int startColor = image[sr][sc] ;
+        vector<vector<int>>ans = image ;
         int drow[] = {-1, 0, +1, 0} ;
         int dcol[] = {0, -1, 0, +1} ;
-        dfs(sr, sc, image, copy, start, color, drow, dcol);
-        return copy ;
+        dfs(sr, sc, image, ans, startColor, color, drow, dcol);
+        return ans ;
     }
 };

8bfdc13a1e8b23684dc27c440232aff0d2db39e4
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-10-22 12:09:09 +0530

    Time: 190 ms (52.06%), Space: 51.5 MB (52.93%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> s(nums.begin(), nums.end()) ;
-        if(s.size() == nums.size()) return false ;
-        return true ;
+        unordered_set<int> s ;
+        for(int i: nums){
+            if(s.count(i)) return true ;
+            s.insert(i) ;
+        }
+        return false ;
     }
 };
 
+// method -2
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         unordered_set<int> s(nums.begin(), nums.end()) ;
+//         if(s.size() == nums.size()) return false ;
+//         return true ;
+//     }
+// };
+
+// method -1 
 // class Solution {
 // public:
 //     bool containsDuplicate(vector<int>& nums) {

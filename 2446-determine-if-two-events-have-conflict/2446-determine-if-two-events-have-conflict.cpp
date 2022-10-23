a9458e7dc05241731af6935c9f9bd4015696b817
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-10-23 10:27:10 +0530

    Time: 11 ms (9.52%), Space: 11.1 MB (47.62%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     bool haveConflict(vector<string>& event1, vector<string>& event2) {
-        if(event1[0] <= event2[1] && event2[0] <= event1[1]) return true ;
-        else return false ;
+        if(max(event1[0], event2[0]) <= min(event1[1], event2[1])) return true ;
+        return false ;
     }
 };
+
+
+// Method - 1
+// class Solution {
+// public:
+//     bool haveConflict(vector<string>& event1, vector<string>& event2) {
+//         if(event1[0] <= event2[1] && event2[0] <= event1[1]) return true ;
+//         return false ;
+//     }
+// };

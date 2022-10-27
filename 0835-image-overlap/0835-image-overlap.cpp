cc3bda23cacd653ccbac22dc63283a684184e1b7
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-10-28 00:01:05 +0530

    Time: 176 ms (50.70%), Space: 12.2 MB (38.19%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -5,13 +5,13 @@ public:
         int N = A.size();
         unordered_map<int, int> count;
         
-        for (int i = 0; i < N * N; ++i)
+        for (int i = 0; i < N * N; ++i){
             if (A[i / N][i % N] == 1)
                 LA.push_back(i / N * 100 + i % N);
-        
-        for (int i = 0; i < N * N; ++i)
+            
             if (B[i / N][i % N] == 1)
                 LB.push_back(i / N * 100 + i % N);
+        }
         
         int res = 0;
         for (int i : LA) 

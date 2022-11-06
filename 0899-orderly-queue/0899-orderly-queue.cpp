ed155bc85221a2c80557afa511be9d159d022fce
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-11-06 22:49:16 +0530

    Time: 15 ms (16.67%), Space: 10.9 MB (32.18%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        string ans = s;
+        if (k == 1) {
+            // "cba" -> "bac" -> "acb" -> "cba" -> ...
+            // we only have N swaps here 
+            // as it will become the original string after N swaps
+            // hence, we can try all N possible swaps and find the lexicographically smallest one
+            for (int i = 1; i < s.size(); i++) {
+               // cout << s.substr(i)<<"  ";
+                //cout << s.substr(0, i)<<"\n";
+                ans = min(ans, s.substr(i) + s.substr(0, i));
+            }
+        } else 
+            sort(ans.begin(),ans.end());
+        
+        return ans;
+    }
+};

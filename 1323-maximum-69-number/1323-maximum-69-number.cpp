28b587afde5c1b5408ac1a75f213c933534bc5a1
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-11-07 20:46:36 +0530

    Time: 0 ms (100.00%), Space: 6 MB (17.66%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -2,11 +2,24 @@ class Solution {
 public:
     int maximum69Number (int num) {
         string s = to_string(num) ;
-        for(char &it: s){
+        for(char &it: s){               // importance of &
             if(it == '6'){
-                it = '9' ; break ;
+                it = '9' ; 
+                break ;
             }
         }
         return stoi(s) ;
     }
 };
+
+
+// Method 2 --> Using Number
+
+// int maximum69Number (int num) {
+//     int temp = num , leftmost_six = -1 ;
+//
+//     for(int i=1; temp > 0; i *= 10, temp /= 10 )
+//          if(temp%10 == 6) leftmost_six = i;
+//
+//     return (leftmost_six == -1) ? num : num + 3 * leftmost_six ;
+// }

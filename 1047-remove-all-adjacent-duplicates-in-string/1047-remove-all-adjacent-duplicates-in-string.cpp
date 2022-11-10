6fef14c4461502990701780226ba5541b6a0719a
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-11-10 20:21:56 +0530

    Time: 55 ms (54.72%), Space: 10.9 MB (66.38%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,19 +1,52 @@
+// Method - 3 using Two pointers
+
+// class Solution {
+// public:
+//     string removeDuplicates(string s) {
+//             int i = 0, n = s.length();
+//             for (int j = 0; j < n; ++j, ++i) {
+//                 s[i] = s[j];
+//                 if (i > 0 && s[i - 1] == s[i]) // count = 2
+//                     i -= 2;
+//             }
+//             return s.substr(0, i);
+//         }
+// };
+
+// Method - 2 using Two pointers
 class Solution {
 public:
     string removeDuplicates(string s) {
-        stack<int> st ;
-        for(char ch: s){
-            if(st.size() > 0 && st.top() == ch)
-                st.pop() ;
+        string temp =  "" ;
+        for(char &ch: s){
+            if(temp.size() && temp.back() == ch)
+                temp.pop_back() ;
             else
-                st.push(ch) ;
+                temp.push_back(ch) ;
         }
-        s = "" ;
-        while(st.size()){
-            s.push_back(st.top()) ;
-            st.pop() ;
-        }
-        reverse(s.begin(), s.end()) ;
-        return s ;
+        return temp ;
     }
 };
+
+
+// Method - 1 using Stack
+
+// class Solution {
+// public:
+//     string removeDuplicates(string s) {
+//         stack<int> st ;
+//         for(char ch: s){
+//             if(st.size() > 0 && st.top() == ch)
+//                 st.pop() ;
+//             else
+//                 st.push(ch) ;
+//         }
+//         s = "" ;
+//         while(st.size()){
+//             s.push_back(st.top()) ;
+//             st.pop() ;
+//         }
+//         reverse(s.begin(), s.end()) ;
+//         return s ;
+//     }
+// };

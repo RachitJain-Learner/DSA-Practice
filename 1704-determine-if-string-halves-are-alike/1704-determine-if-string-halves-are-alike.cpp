b32352bb39c3acb75c72e52863cff39eb22b9e4f
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-12-02 00:30:03 +0530

    Time: 2 ms (89.18%), Space: 6.7 MB (56.35%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -5,17 +5,18 @@ public:
         int i=0 , j = n / 2 ;
         int cnt1 = 0 , cnt2 = 0 ;
         
-        while(i < j){
+        while(i < n){
             if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' ||s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
-                cnt1++ ;
+                if(i < n/2) cnt1++ ;
+                else cnt2++ ;
             i++ ;
         }
         
-        while(i < n){
-            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' ||s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' )
-                cnt2++ ;
-            i++ ;
-        }
+        // while(i < n){
+        //     if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' ||s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' )
+        //         cnt2++ ;
+        //     i++ ;
+        // }
         
         return cnt1 == cnt2 ;
         

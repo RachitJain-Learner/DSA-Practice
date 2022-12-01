a7bc8b3ab1ea43d4ab3846156eb9af7814e09071
RachitJain-Learner <91320240+RachitJain-Learner@users.noreply.github.com>
2022-12-02 00:32:47 +0530

    Time: 6 ms (58.35%), Space: 6.6 MB (83.41%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -6,19 +6,28 @@ public:
         int cnt1 = 0 , cnt2 = 0 ;
         
         while(i < n){
-            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' ||s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+            class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int n = s.length() ;
+        int i=0 , j = n / 2 ;
+        int cnt1 = 0 , cnt2 = 0 ;
+        
+        while(i < n){
+            s[i]=tolower(s[i]);
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i]=='u')
                 if(i < n/2) cnt1++ ;
                 else cnt2++ ;
             i++ ;
         }
-        
-        // while(i < n){
-        //     if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' ||s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' )
-        //         cnt2++ ;
-        //     i++ ;
-        // }
-        
         return cnt1 == cnt2 ;
-        
+    }
+};
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i]=='u' || s[i] == 'A' ||s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+                if(i < n/2) cnt1++ ;
+                else cnt2++ ;
+            i++ ;
+        }
+        return cnt1 == cnt2 ;
     }
 };
